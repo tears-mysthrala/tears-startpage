aef44349a55ee15cb608757d2ebfde77b1938ac7
Tears Mysthrala <unaiup@gmail.com>
2024-05-25 15:08:34 +0530

    Update userconfig.js

--- userconfig.js
+++ userconfig.js
@@ -3,7 +3,7 @@ let saved_config = JSON.parse(localStorage.getItem("CONFIG"));
 const default_config = {
   overrideStorage: true,
   temperature: {
-    location: 'Matão, São Paulo',
+    location: 'Elgoibar, Espain',
     scale: "C",
   },
   clock: {
